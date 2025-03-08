1ac5c18a260cd13854d6cbadb6567f192ee01928
RyanMurphy0 <rdmurph@outlook.com>
2025-03-08 12:20:11 -0500

    whole thing

---
+++ models.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class EmojiPair(BaseModel):
+    emoji1: str = Field(..., description="First emoji")
+    emoji2: str = Field(..., description="Second emoji")
+
+class Superhero(BaseModel):
+    name: str = Field(..., description = "name of the superhero")
+    emoji_pair: EmojiPair = Field(..., description="Emoji pair that led to this guess")
+
+class UserRecord(BaseModel):
+    superheroes: List[Superhero] = Field(default_factory=list)
+
+    def add_superhero(self, superhero: Superhero) -> None:
+        self.superheroes.append(superhero)
